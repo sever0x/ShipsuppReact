0fe0e8c028a99ac750bec52c74dd7ba52ca2fda9
shdwraze <shdwrazedev@gmail.com>
2024-07-14 01:29:14 +0300

    changes: profile

--- src/pages/profile/containers/Profile.tsx
+++ src/pages/profile/containers/Profile.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Avatar, Box, Button, CircularProgress, Container, Typography} from '@mui/material';
+import {Avatar, Box, Button, CircularProgress, Container, Grid, Divider} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 import useAuth from 'misc/hooks/useAuth';
 import pageURLs from 'constants/pagesURLs';
@@ -8,9 +8,12 @@ import * as pages from 'constants/pages';
 import {fetchUserProfile} from '../actions/profileActions';
 import {RootState} from 'app/reducers';
 import EditProfile from '../components/EditProfile';
+import EditIcon from '@mui/icons-material/Edit';
+import Stack from 'components/Stack';
+import Typography from 'components/Typography';
 
 const Profile: React.FC = () => {
-    const { user, logout } = useAuth();
+    const {user, logout} = useAuth();
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const profile = useSelector((state: RootState) => state.profile);
@@ -32,7 +35,7 @@ const Profile: React.FC = () => {
     }
 
     if (profile.loading) {
-        return <CircularProgress />;
+        return <CircularProgress/>;
     }
 
     if (profile.error) {
@@ -40,43 +43,66 @@ const Profile: React.FC = () => {
     }
 
     return (
-        <Container maxWidth="sm">
+        <Container maxWidth="md" sx={{mt: 4}}>
             {isEditing ? (
-                <EditProfile onCancel={() => setIsEditing(false)} />
+                <EditProfile onCancel={() => setIsEditing(false)}/>
             ) : (
                 <>
-                    <Box display="flex" flexDirection="column" alignItems="center" mb={3}>
-                        <Box position="relative">
-                            <Avatar
-                                src={profile.data?.profilePhoto}
-                                alt={`${profile.data?.firstName} ${profile.data?.lastName}`}
-                                sx={{ width: 100, height: 100, mb: 2 }}
-                            />
-                        </Box>
-                        <Typography variant="h4" gutterBottom>User Profile</Typography>
-                    </Box>
-                    <Typography variant="body1">Email: {user.email}</Typography>
-                    <Typography variant="body1">UID: {user.uid}</Typography>
-                    {profile.data && (
-                        <>
-                            <Typography variant="body1">First Name: {profile.data.firstName}</Typography>
-                            <Typography variant="body1">Last Name: {profile.data.lastName}</Typography>
-                            <Typography variant="body1">Phone: {profile.data.phone}</Typography>
-                            {profile.data.port && (
-                                <Typography variant="body1">
-                                    Port: {profile.data.port.title} ({profile.data.port.city.title}, {profile.data.port.city.country.title})
+                    <Stack direction="row" justifyContent="space-between" alignItems="center">
+                        <Stack direction="row" spacing={3} alignItems="center">
+                            <Box position="relative">
+                                <Avatar
+                                    src={profile.data?.profilePhoto}
+                                    alt={`${profile.data?.firstName} ${profile.data?.lastName}`}
+                                    sx={{width: 100, height: 100}}
+                                />
+                            </Box>
+                            <Box>
+                                <Typography
+                                    variant="h4">{`${profile.data?.firstName} ${profile.data?.lastName}`}</Typography>
+                                <Typography variant="body1" color="secondary">
+                                    {profile.data.port.title} ({profile.data.port.city.title}, {profile.data.port.city.country.title})
                                 </Typography>
-                            )}
-                        </>
-                    )}
-                    <Box mt={3}>
-                        <Button variant="contained" color="primary" onClick={() => setIsEditing(true)} sx={{ mr: 2 }}>
-                            Edit Profile
-                        </Button>
-                        <Button variant="outlined" color="secondary" onClick={handleLogout}>
-                            Logout
-                        </Button>
-                    </Box>
+                            </Box>
+                        </Stack>
+                        <Box>
+                            <Button
+                                variant="outlined"
+                                startIcon={<EditIcon/>}
+                                onClick={() => setIsEditing(true)}
+                            >
+                                Edit Profile
+                            </Button>
+                        </Box>
+                    </Stack>
+                    <Divider sx={{mt: 3}}/>
+                    <Stack sx={{mt: 3}}>
+                        <Typography variant="h5" gutterBottom bold={true}>Information</Typography>
+                        <Grid container spacing={3} sx={{mt: 1}}>
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="body2" color="secondary">Email</Typography>
+                                <Typography variant="body1">{user.email}</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="body2" color="secondary">UID</Typography>
+                                <Typography variant="body1">{user.uid}</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="body2" color="secondary">Full Name</Typography>
+                                <Typography
+                                    variant="body1">{profile.data?.firstName} {profile.data?.lastName}</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Typography variant="body2" color="secondary">Phone</Typography>
+                                <Typography variant="body1">{profile.data?.phone || 'Not provided'}</Typography>
+                            </Grid>
+                        </Grid>
+                        <Box mt={4}>
+                            <Button variant="contained" color="secondary" onClick={handleLogout}>
+                                Logout
+                            </Button>
+                        </Box>
+                    </Stack>
                 </>
             )}
         </Container>
