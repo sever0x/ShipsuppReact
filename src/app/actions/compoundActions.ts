638943e485aabf60b4d1175ad18c0bc3ee6d9885
shdwraze <shdwrazedev@gmail.com>
2024-07-27 16:28:58 +0300

    fixed bug [7]

---
+++ src/app/actions/compoundActions.ts
@@ -0,0 +1,23 @@
+import {ThunkAction} from 'redux-thunk';
+import {RootState} from '../reducers';
+import {UnknownAction} from 'redux';
+import actions from "../../app/actions/userAuth";
+import {fetchUserProfile} from "pages/profile/actions/profileActions";
+
+export const signInAndLoadProfile = (email: string, password: string): ThunkAction<Promise<void>, RootState, unknown, UnknownAction> =>
+    async (dispatch) => {
+        await dispatch(actions.fetchLogin(email, password) as any);
+        const user = JSON.parse(localStorage.getItem('safeUser') ?? '{}');
+        if (user.uid) {
+            await dispatch(fetchUserProfile(user.uid) as any);
+        }
+    };
+
+export const googleSignInAndLoadProfile = (): ThunkAction<Promise<void>, RootState, unknown, UnknownAction> =>
+    async (dispatch) => {
+        await dispatch(actions.fetchGoogleSignIn());
+        const user = JSON.parse(localStorage.getItem('safeUser') ?? '{}');
+        if (user.uid) {
+            await dispatch(fetchUserProfile(user.uid) as any);
+        }
+    };
