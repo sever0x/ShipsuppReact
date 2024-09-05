4790128f15f5d40f2f6f5c69af97fad6cc2a9d15
shdwraze <shdwrazedev@gmail.com>
2024-09-05 19:15:34 +0300

    DONE: [18] Mismatch in Product Name in Orders List and Order View

--- src/pages/orders/reducers/orderReducer.ts
+++ src/pages/orders/reducers/orderReducer.ts
@@ -4,14 +4,17 @@ import {
     FETCH_SELLER_ORDERS_FAILURE,
     UPDATE_ORDER_STATUS_REQUEST,
     UPDATE_ORDER_STATUS_SUCCESS,
-    UPDATE_ORDER_STATUS_FAILURE, FETCH_ORDER_DETAILS_REQUEST, FETCH_ORDER_DETAILS_SUCCESS, FETCH_ORDER_DETAILS_FAILURE
+    UPDATE_ORDER_STATUS_FAILURE,
+    FETCH_ORDER_DETAILS_REQUEST,
+    FETCH_ORDER_DETAILS_SUCCESS,
+    FETCH_ORDER_DETAILS_FAILURE
 } from '../constants/actionTypes';
-import {Order} from "pages/orders/types/Order";
+import { Order } from "pages/orders/types/Order";
 
 interface OrderState {
     loadingOrders: boolean;
     loadingDetails: boolean;
-    data: any[];
+    data: Order[];
     orderDetails: Order | null;
     error: string | null;
 }
@@ -43,7 +46,17 @@ const orderReducer = (state = initialState, action: any): OrderState => {
                 error: null
             };
         case FETCH_ORDER_DETAILS_SUCCESS:
-            return { ...state, loadingDetails: false, orderDetails: action.payload, error: null };
+            return {
+                ...state,
+                loadingDetails: false,
+                orderDetails: action.payload,
+                data: state.data.map(order =>
+                    order.id === action.payload.id
+                        ? { ...order, good: { ...order.good, ...action.payload.good } }
+                        : order
+                ),
+                error: null
+            };
         case FETCH_SELLER_ORDERS_FAILURE:
             return { ...state, loadingOrders: false, error: action.payload };
         case UPDATE_ORDER_STATUS_FAILURE:
