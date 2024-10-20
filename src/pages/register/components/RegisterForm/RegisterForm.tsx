f1193628d2a35571db39517cdbc00995db9eb0db
shdwraze <shdwrazedev@gmail.com>
2024-10-21 02:57:38 +0300

    CHANGES: refactor RegisterForm.tsx

--- src/pages/register/components/RegisterForm/RegisterForm.tsx
+++ src/pages/register/components/RegisterForm/RegisterForm.tsx
@@ -1,19 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {useLocation, useNavigate} from 'react-router-dom';
-import {createUseStyles} from 'react-jss';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { createUseStyles } from 'react-jss';
 import useAuth from 'misc/hooks/useAuth';
 import Typography from 'components/Typography';
-import {Link} from "@mui/material";
+import { Link } from "@mui/material";
 import pageURLs from 'constants/pagesURLs';
 import * as pages from 'constants/pages';
 import actions from "../../../../misc/actions/portsActions";
 import EmailField from 'components/EmailField';
-import PasswordField from 'components/PasswordField';
 import TextField from 'components/TextField';
 import SubmitButton from 'components/SubmitButton';
-import {useSelector} from "react-redux";
-import {RootState} from 'app/reducers';
-import {useAppDispatch} from 'misc/hooks/useAppDispatch';
+import { useSelector } from "react-redux";
+import { RootState } from 'app/reducers';
+import { useAppDispatch } from 'misc/hooks/useAppDispatch';
 import PortSelector from 'components/PortSelector';
 
 const getClasses = createUseStyles(() => ({
@@ -40,23 +39,15 @@ const getClasses = createUseStyles(() => ({
     },
 }));
 
-interface LocationState {
-    step?: number;
-    email?: string;
-    firstName?: string;
-    lastName?: string;
-}
-
 const RegisterForm: React.FC = () => {
     const classes = getClasses();
-    const { register, googleSignIn, updateProfile, user, error } = useAuth();
+    const { register, updateProfile, user, error } = useAuth();
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const ports = useSelector((state: RootState) => state.ports.data);
     const location = useLocation();
-    const state = location.state as LocationState;
+    const state = location.state;
 
-    const [step, setStep] = useState(state?.step ?? 1);
     const [email, setEmail] = useState(state?.email ?? '');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -67,14 +58,14 @@ const RegisterForm: React.FC = () => {
     const [vesselMMSI, setVesselMMSI] = useState('');
     const [selectedPort, setSelectedPort] = useState<string>('');
     const [isGoogleSignIn, setIsGoogleSignIn] = useState(false);
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
     useEffect(() => {
         dispatch(actions.fetchPorts());
     }, [dispatch]);
 
     useEffect(() => {
-        if (state?.step) {
-            setStep(state.step);
+        if (state) {
             setEmail(state.email || '');
             setFirstName(state.firstName || '');
             setLastName(state.lastName || '');
@@ -82,17 +73,12 @@ const RegisterForm: React.FC = () => {
         }
     }, [state]);
 
-    const handleNextStep = (event: React.FormEvent) => {
+    const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
-        if (step === 1 && password !== confirmPassword) {
+        if (password !== confirmPassword) {
             alert("Passwords do not match!");
             return;
         }
-        setStep(step + 1);
-    };
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
         try {
             const selectedPortObject = ports[selectedPort];
             if (isGoogleSignIn && user) {
@@ -114,104 +100,69 @@ const RegisterForm: React.FC = () => {
                     portsArray: selectedPortObject ? [selectedPortObject] : []
                 });
             }
+            setSuccessMessage("Thank you! Your request has been successfully submitted. We will contact you shortly.");
         } catch (error) {
             if (error instanceof Error) {
-                if (error.message.includes('successfully submitted')) {
-                    alert(error.message);
-                    navigate('/login');
-                } else {
-                    console.error("Registration error:", error);
-                }
+                console.error("Registration error:", error);
             } else {
                 console.error("Unknown error:", error);
             }
         }
-
     };
 
     const handlePortSelect = (portId: string) => {
         setSelectedPort(portId);
     };
 
-    const renderStep = () => {
-        switch (step) {
-            case 1:
-                return (
-                    <>
-                        <EmailField email={email} setEmail={setEmail} />
-                        <PasswordField password={password} setPassword={setPassword} />
-                        <PasswordField
-                            password={confirmPassword}
-                            setPassword={setConfirmPassword}
-                            placeholder="Confirm Password"
-                        />
-                    </>
-                );
-            case 2:
-                return (
-                    <>
-                        <TextField
-                            label="First Name"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                            required
-                        />
-                        <TextField
-                            label="Last Name"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            required
-                        />
-                        <TextField
-                            label="Phone"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                            required
-                        />
-                    </>
-                );
-            case 3:
-                return (
-                    <>
-                        <div className={classes.portSelectorContainer}>
-                            <PortSelector
-                                ports={ports}
-                                selectedPorts={selectedPort ? [selectedPort] : []}
-                                onPortSelect={handlePortSelect}
-                                multiSelect={false}
-                                label="Select a port"
-                            />
-                        </div>
-                    </>
-                );
-            default:
-                return null;
-        }
-    };
-
     return (
-        <>
-            <form onSubmit={step === 3 ? handleSubmit : handleNextStep}>
-                <div className={classes.textContainer}>
-                    <Typography sx={{fontSize: '2.25rem', fontWeight: 'bold'}}>
-                        Sign Up
-                    </Typography>
-                    <Typography sx={{paddingTop: '16px'}}>
-                        Already have an account? <Link href={`${pageURLs[pages.login]}`} sx={{
-                        color: 'inherit',
-                        textDecorationColor: 'inherit'
-                    }}>Sign In</Link>
-                    </Typography>
+        <form onSubmit={handleSubmit}>
+            <div className={classes.textContainer}>
+                <Typography sx={{ fontSize: '2.25rem', fontWeight: 'bold' }}>
+                    Become a partner
+                </Typography>
+                <Typography sx={{ paddingTop: '16px' }}>
+                    Already have an account? <Link href={`${pageURLs[pages.login]}`} sx={{
+                    color: 'inherit',
+                    textDecorationColor: 'inherit'
+                }}>Sign In</Link>
+                </Typography>
+            </div>
+            <div className={classes.fieldsContainer}>
+                <EmailField email={email} setEmail={setEmail} />
+                <TextField
+                    label="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                />
+                <TextField
+                    label="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                />
+                <TextField
+                    label="Phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    required
+                />
+                <div className={classes.portSelectorContainer}>
+                    <PortSelector
+                        ports={ports}
+                        selectedPorts={selectedPort ? [selectedPort] : []}
+                        onPortSelect={handlePortSelect}
+                        multiSelect={false}
+                        label="Select a port"
+                    />
                 </div>
-                <div className={classes.fieldsContainer}>
-                    {renderStep()}
-                </div>
-                {error && <Typography color="error">{error.message}</Typography>}
-                <div className={classes.buttonsContainer}>
-                    <SubmitButton text={step === 3 ? "Sign Up" : "Next"} />
-                </div>
-            </form>
-        </>
+            </div>
+            {error && <Typography color="error">{error.message}</Typography>}
+            {successMessage && <Typography color="success">{successMessage}</Typography>}
+            <div className={classes.buttonsContainer}>
+                <SubmitButton text="Send request" />
+            </div>
+        </form>
     );
 };
 
